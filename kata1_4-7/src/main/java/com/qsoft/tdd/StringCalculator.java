b4b3dfa04bce73dc3d8d9ed80941168378ed9251
hunter lee <huynhlbq@gmail.com>
2013-07-04 14:07:13 +0700

    finish day 2

--- kata1_4-7/src/main/java/com/qsoft/tdd/StringCalculator.java
+++ kata1_4-7/src/main/java/com/qsoft/tdd/StringCalculator.java
@@ -6,15 +6,32 @@ package com.qsoft.tdd;
  */
 public class StringCalculator
 {
-    public int add(String s)
+    private static final String DELIMITER = "(,|\n|;|\\*|\\]|\\[|\\/)";
+
+    public int add(String numbers) throws Exception
     {
-        if (s.isEmpty())
+        if (numbers.isEmpty())
         {
             return 0;
         }
-        else
-        {
 
+        int total = 0;
+        for (String param : numbers.split(DELIMITER))
+        {
+            if (numbers.contains("-"))
+            {
+                throw new Exception("negatives not allowed");
+            }
+
+            if (!param.isEmpty())
+            {
+                int number = Integer.parseInt(param);
+                if (number < 1000)
+                {
+                    total += number;
+                }
+            }
         }
+        return total;
     }
 }
--- kata1_4-7/src/test/java/com/qsoft/tdd/StringCalculatorTest.java
+++ kata1_4-7/src/test/java/com/qsoft/tdd/StringCalculatorTest.java
@@ -12,37 +12,59 @@ import org.junit.Test;
 public class StringCalculatorTest
 {
     @Test
-    public void addEmptyStringTest()
+    public void addEmptyStringTest() throws Exception
     {
         StringCalculator calculator = new StringCalculator();
         Assert.assertEquals(0, calculator.add(""));
     }
 
     @Test
-    public void addMultiParamsTest()
+    public void addMultiParamsTest() throws Exception
     {
         StringCalculator calculator = new StringCalculator();
         Assert.assertEquals(20, calculator.add("1,2,7,10"));
     }
 
     @Test
-    public void addWithNewLineTest()
+    public void addWithNewLineTest() throws Exception
     {
         StringCalculator calculator = new StringCalculator();
-        Assert.assertEquals(6, calculator.add("1\\n2,3"));
+        Assert.assertEquals(6, calculator.add("1\n2,3"));
     }
 
     @Test
-    public void addMoreDelimiterTest()
+    public void addMoreDelimiterTest() throws Exception
     {
         StringCalculator calculator = new StringCalculator();
         Assert.assertEquals(3, calculator.add("//;\n1;2"));
     }
 
     @Test(expected = Exception.class)
-    public void addNegativeTest()
+    public void addNegativeTest() throws Exception
     {
         StringCalculator calculator = new StringCalculator();
         calculator.add("2;-3,4");
     }
+
+    @Test
+    public void addGT1000Test() throws Exception
+    {
+        StringCalculator calculator = new StringCalculator();
+        Assert.assertEquals(2, calculator.add("2,1001"));
+    }
+
+    @Test
+    public void addWithAnyDelimiterLengthTest() throws Exception
+    {
+        StringCalculator calculator = new StringCalculator();
+        Assert.assertEquals(6, calculator.add("//[***]\n1***2***3"));
+    }
+
+    @Test
+    public void addWithDefinedDelimiterTest() throws Exception
+    {
+        StringCalculator calculator = new StringCalculator();
+        Assert.assertEquals(6, calculator.add("//[*][%]\\n1*2%3"));
+    }
+
 }
