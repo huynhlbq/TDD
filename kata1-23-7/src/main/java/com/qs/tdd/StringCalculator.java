8cd604ebed886b58d8d431afc02d9425f43b1466
hunter lee <huynhlbq@gmail.com>
2013-07-23 16:48:34 +0700

    passed custom define

--- kata1-23-7/src/main/java/com/qs/tdd/StringCalculator.java
+++ kata1-23-7/src/main/java/com/qs/tdd/StringCalculator.java
@@ -7,17 +7,30 @@ package com.qs.tdd;
 public class StringCalculator
 {
     private static final String DEFAULT_DELIMITER_REGEX = ",|\n";
+    private static final String CUSTOM_DEFINE = "//";
 
     public static int add(String numbers)
     {
+        String delimiter = DEFAULT_DELIMITER_REGEX;
         if (numbers.isEmpty())
         {
             return 0;
         }
 
-        int total = 0;
-        for (String param : numbers.split(DEFAULT_DELIMITER_REGEX))
+        if (numbers.startsWith(CUSTOM_DEFINE))
         {
+            String customDefineDelimiter = numbers.substring(CUSTOM_DEFINE.length(), CUSTOM_DEFINE.length() + 1);
+            delimiter += "|" + customDefineDelimiter;
+            numbers = numbers.substring(numbers.lastIndexOf("\n"));
+        }
+
+        int total = 0;
+        for (String param : numbers.split(delimiter))
+        {
+            if (param.isEmpty())
+            {
+                continue;
+            }
             int paramNumber = Integer.parseInt(param);
             total += paramNumber;
         }
