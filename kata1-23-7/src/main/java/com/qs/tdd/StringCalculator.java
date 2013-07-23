1d8c6902b125d210d63355881601e3ec1d9fe227
hunter lee <huynhlbq@gmail.com>
2013-07-23 18:17:49 +0700

    passed
    Signed-off-by: hunter lee <huynhlbq@gmail.com>

--- kata1-23-7/src/main/java/com/qs/tdd/StringCalculator.java
+++ kata1-23-7/src/main/java/com/qs/tdd/StringCalculator.java
@@ -8,12 +8,30 @@ import java.lang.String;
  */
 public class StringCalculator
 {
+    private static final String DEFAULT_DELIMITER_REGEX = ",";
+
     public static int add(String numbers)
     {
         if (numbers.isEmpty())
         {
             return 0;
         }
-        return Integer.parseInt(numbers);
+        String[] numbersString = tokenizer(numbers);
+        int total = 0;
+        for (String aNumbersString : numbersString)
+        {
+            total += string2Int(aNumbersString);
+        }
+        return total;
+    }
+
+    private static int string2Int(String s)
+    {
+        return Integer.parseInt(s);
+    }
+
+    private static String[] tokenizer(String numbers)
+    {
+        return numbers.split(DEFAULT_DELIMITER_REGEX);
     }
 }
