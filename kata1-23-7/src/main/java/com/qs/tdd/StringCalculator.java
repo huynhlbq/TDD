9ce6e09609a764f0bcc4c377f43a87212b1e07b8
hunter lee <huynhlbq@gmail.com>
2013-07-23 18:10:15 +0700

    add test 2

--- kata1-23-7/src/main/java/com/qs/tdd/StringCalculator.java
+++ kata1-23-7/src/main/java/com/qs/tdd/StringCalculator.java
@@ -1,9 +1,15 @@
 package com.qs.tdd;
 
+import java.lang.String;
+
 /**
  * User: Hunter
  * Date: 7/23/13
  */
 public class StringCalculator
 {
+    public static int add(String numbers)
+    {
+        return 0;
+    }
 }
--- kata1-23-7/src/test/java/com/qs/tdd/StringCalculatorTest.java
+++ kata1-23-7/src/test/java/com/qs/tdd/StringCalculatorTest.java
@@ -1,9 +1,23 @@
 package com.qs.tdd;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 /**
  * User: Hunter
  * Date: 7/23/13
  */
 public class StringCalculatorTest
 {
+    @Test
+    public void testAddEmptyString()
+    {
+        assertEquals(0, StringCalculator.add(""));
+    }
+
+    @Test
+    public void testAddOneNumber()
+    {
+        assertEquals(2, StringCalculator.add("2"));
+    }
 }
