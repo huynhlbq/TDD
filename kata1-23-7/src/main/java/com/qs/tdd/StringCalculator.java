0a3464ad884e86ea4624a2675dc9a965893a373e
hunter lee <huynhlbq@gmail.com>
2013-07-23 16:05:32 +0700

    make test 2 passed

--- kata1-23-7/src/main/java/com/qs/tdd/StringCalculator.java
+++ kata1-23-7/src/main/java/com/qs/tdd/StringCalculator.java
@@ -8,6 +8,10 @@ public class StringCalculator
 {
     public static int add(String numbers)
     {
+        if (numbers.isEmpty())
+        {
             return 0;
+        }
+        return Integer.parseInt(numbers);
     }
 }
