0b4cfa3899b986ec2f5d3382611983e81f36c0c7
hunter lee <huynhlbq@gmail.com>
2013-07-23 18:24:00 +0700

    add

--- kata1-23-7/src/test/java/com/qs/tdd/StringCalculatorTest.java
+++ kata1-23-7/src/test/java/com/qs/tdd/StringCalculatorTest.java
@@ -32,4 +32,11 @@ public class StringCalculatorTest
     {
         assertEquals(6, StringCalculator.add("1\n2,3"));
     }
+
+    @Test
+    public void testWithCustomDelimiter() throws Exception
+    {
+        assertEquals(3, StringCalculator.add("//;\n1;2"));
+    }
+
 }
