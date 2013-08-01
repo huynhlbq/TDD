98fb228575787f2eb6e2ef957b67c5233407e6e1
hunter lee <huynhlbq@gmail.com>
2013-08-01 12:46:43 +0700

    init project

---
+++ Kata4/SLL/src/test/java/com/qs/tdd/SingleLinkedListTest.java
@@ -0,0 +1,17 @@
+package com.qs.tdd;
+
+import org.junit.Test;
+
+/**
+ * User: Hunter
+ * Date: 8/1/13
+ */
+public class SingleLinkedListTest
+{
+    @Test
+    public void testCreated()
+    {
+
+    }
+
+}
