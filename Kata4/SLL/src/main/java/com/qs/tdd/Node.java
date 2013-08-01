9efabc06d09dbc7032482fd069301950598b1913
hunter lee <huynhlbq@gmail.com>
2013-08-01 22:00:58 +0700

    Add Node class

---
+++ Kata4/SLL/src/main/java/com/qs/tdd/Node.java
@@ -0,0 +1,31 @@
+package com.qs.tdd;
+
+/**
+ * User: Hunter
+ * Date: 8/1/13
+ */
+public class Node
+{
+    private String value;
+    private int next;
+
+    public String getValue()
+    {
+        return value;
+    }
+
+    public void setValue(String value)
+    {
+        this.value = value;
+    }
+
+    public int getNext()
+    {
+        return next;
+    }
+
+    public void setNext(int next)
+    {
+        this.next = next;
+    }
+}
