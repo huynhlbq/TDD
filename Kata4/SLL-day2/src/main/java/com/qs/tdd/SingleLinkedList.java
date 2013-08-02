e515ed88f2e5af71be469ffa29facda3887d4b5b
hunter lee <huynhlbq@gmail.com>
2013-08-02 08:47:55 +0700

    implement add all objects to list

--- Kata4/SLL-day2/src/main/java/com/qs/tdd/SingleLinkedList.java
+++ Kata4/SLL-day2/src/main/java/com/qs/tdd/SingleLinkedList.java
@@ -28,6 +28,63 @@ public class SingleLinkedList
         addAll(objects);
     }
 
+    private boolean addAll(List objects)
+    {
+        return addAll(size, objects);
+    }
+
+    /**
+     * add all objects from index
+     *
+     * @param index
+     * @param objects
+     * @return
+     */
+    private boolean addAll(int index, List objects)
+    {
+        if (index < 0 || index > size)
+        {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        if (objects.size() == 0)
+        {
+            return false;
+        }
+        Node successor = (index == size ? header : entry(index));
+        Node predecessor = successor.getPrevious();
+        for (Object o : objects)
+        {
+            Node e = new Node(successor, predecessor, o);
+            predecessor.setNext(e);
+            predecessor = e;
+        }
+        successor.setPrevious(predecessor);
+
+        size += objects.size();
+        return true;
+    }
+
+    private Node entry(int index)
+    {
+        Node e = header;
+        if (index < (size >> 1))
+        {
+            for (int i = 0; i <= index; i++)
+            {
+                e = e.getNext();
+            }
+        }
+        else
+        {
+            for (int i = size; i > index; i--)
+            {
+                e = e.getPrevious();
+            }
+        }
+        return e;
+    }
+
 // -------------------------- OTHER METHODS --------------------------
 
     public int size()
