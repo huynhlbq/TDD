d681ae1142d469055f05d06a0c6acb7f892552e8
hunter lee <huynhlbq@gmail.com>
2013-08-02 09:25:38 +0700

    clear comment

--- Kata4/SLL-day2/src/main/java/com/qs/tdd/SingleLinkedList.java
+++ Kata4/SLL-day2/src/main/java/com/qs/tdd/SingleLinkedList.java
@@ -150,6 +150,6 @@ public class SingleLinkedList
                 return entry(i);
             }
         }
-        return null;  //To change body of created methods use File | Settings | File Templates.
+        return null;
     }
 }
