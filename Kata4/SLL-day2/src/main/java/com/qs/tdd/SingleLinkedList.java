f3a93e3d61af9c9d4f75a341e427fc46d5f0a5dc
hunter lee <huynhlbq@gmail.com>
2013-08-02 08:29:36 +0700

    - add size variable and header node - create constructor for init SLL

--- Kata4/SLL-day2/src/main/java/com/qs/tdd/SingleLinkedList.java
+++ Kata4/SLL-day2/src/main/java/com/qs/tdd/SingleLinkedList.java
@@ -1,13 +1,37 @@
 package com.qs.tdd;
 
+import java.util.List;
+
 /**
  * User: Hunter
  * Date: 8/2/13
  */
 public class SingleLinkedList
 {
+// ------------------------------ FIELDS ------------------------------
+
+    //header node
+    private Node header = new Node(null, null, null);
+    private int size = 0;
+
+// --------------------------- CONSTRUCTORS ---------------------------
+
+    public SingleLinkedList()
+    {
+        header.setNext(header);
+        header.setPrevious(header);
+    }
+
+    public SingleLinkedList(List objects)
+    {
+        this();
+        addAll(objects);
+    }
+
+// -------------------------- OTHER METHODS --------------------------
+
     public int size()
     {
-        return 0;
+        return size;
     }
 }
